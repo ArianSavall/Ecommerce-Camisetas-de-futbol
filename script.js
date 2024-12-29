fd2cb89c2a00ed75da1c01e054e4e3ae2e71f3c6
savallarian45@gmail.com <savallarian45@gmail.com>
2024-12-29 20:45:18 -0300

    Implementado el botón de + info de productos

--- script.js
+++ script.js
@@ -17,22 +17,43 @@ function validateContactForm(){
     }   
 }
 
-function getAllCamisetasFromJson(){
-    fetch("camisetas.json").then((response)=> response.json()).then((data) => {
-        const camisetasContainer = document.getElementById("camisetas-container");
-        data.forEach(camiseta => {
-            camisetasContainer.innerHTML += `
-            <div class="card mx-3" style="width: 18rem;">
-                        <img src="${camiseta.image}" class="card-img-top" alt="${camiseta.title}">
-                        <div class="card-body m-2">
-                        <h5 class="card-title">${camiseta.title}</h5>
-                        <p class="card-text">Precio: $${camiseta.price} </p>
-                        <p class="card-text">Talles: ${camiseta.sizes}</p>
-                        <button class="btn btn-primary">Agregar al carrito</button>
-                        <button class="btn btn-secondary">+Info</button>
-                        </div>
-                    </div>
-            `
-        });
-    }).catch((error) => console.error("Error al obtener las camisetas:", error));
+
+document.addEventListener("DOMContentLoaded" , (event) => {fetch("camisetas.json").then((response)=> response.json()).then((data) => {
+    const camisetasContainer = document.getElementById("camisetas-container");
+    data.forEach(camiseta => {
+        camisetasContainer.innerHTML += `
+        <div class="card mx-3" style="width: 18rem;">
+            <img src="${camiseta.image}" class="card-img-top" alt="${camiseta.title}">
+            <div class="card-body m-2">
+                <h5 id="titulo${camiseta.id}" class="card-title">${camiseta.title} ${camiseta.id}</h5>
+                <p id="parrafoPrecio${camiseta.id}" class="card-text">Precio: $${camiseta.price} </p>
+                <p id="parrafoTalles${camiseta.id}" class="card-text">Talles: ${camiseta.sizes}</p>
+                <div id="descripcion${camiseta.id}" class = "container">
+                    <output id="descText${camiseta.id}"  class = "descripcion my-3">  ${camiseta.descripcion} </output>
+                </div>
+                <div class="container">
+                    <button id="botonCompra${camiseta.id}" class="btn btn-primary">Agregar al carrito</button>
+                    <button onclick="mostrarMasInfo(${camiseta.id})" id="botonInfo${camiseta.id}" class="btn btn-secondary">+Info</button>
+                </div>
+             </div>  
+        </div>
+        `
+    });
+}).catch((error) => console.error("Error al obtener las camisetas:", error));});
+
+function mostrarMasInfo(id){
+
+    var cardTitle = document.getElementById("title" + id);
+    var idString = id.toString();
+    var idDescripcion = "descripcion" + id;
+    var idText = "descText" + id;
+    if(document.getElementById(idDescripcion).style.display == "block"){
+        document.getElementById(idDescripcion).style.display = "none";
+        document.getElementById(idText).style.display = "none";
+    }else{
+        document.getElementById(idDescripcion).style.display = "block";
+        document.getElementById(idText).style.display = "block";
+    }
 }
+
+
